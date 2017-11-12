9b33a5ec9a62183015ac5ecf0b0a605e54b80ba3
lucy3 <lucy3@stanford.edu>
2017-11-12 14:56:10 -0800

    incomplete

--- code/lexical_features.py
+++ code/lexical_features.py
@@ -1,11 +1,19 @@
 """
-Contains a function that returns 
-a dictionary of comment id
-to an array of features. 
+Creates lexical features
+
+Should be parallelized:
+xargs -a /dfs/dataset/infolab/Reddit/comments/2015/RC_2015-05 -0 -d '\n' -P 144 -n 1 python lexical_features.py
+
+Output is named
+linkid_commentid.npy
 """
+import json
+import numpy
+import sys
 
 POST_IDs = "../data/post_IDs.txt"
 INPUT = "/dfs/dataset/infolab/Reddit/comments/2015/RC_2015-05"
+OUTPUT = "../logs/lexical_vectors"
 
 def get_text():
     '''
@@ -14,12 +22,20 @@ def get_text():
     @return
         - 
     '''
-    with open(POST_IDS, 'r') as 
+    with open(POST_IDs, 'r') as post_id_file: 
+        post_ids = set(post_id_file.read().split())
+    i = 0
     with open(INPUT, 'r') as input_file: 
         for line in input_file: 
             comment = json.loads(line)
+            post = comment['link_id'].split('_')[-1]
+            if post in post_ids: 
+                i += 1
+    print i
+            #comment['body']
+    
 
-def get_lexical_features():
+def get_lexical_features(text, post_id, comment_id):
     '''
     @input
         - 
@@ -29,8 +45,13 @@ def get_lexical_features():
     pass
 
 def main():
-    get_text()
-    get_lexical_features()
+    with open(POST_IDs, 'r') as post_id_file: 
+        post_ids = set(post_id_file.read().split())
+    comment = json.loads(sys.argv[1])
+    post = comment['link_id'].split('_')[-1]
+    if post in post_ids: 
+        get_lexical_features(comment['body'], post, comment['id'])
+    #get_text()
 
 if __name__ == "__main__":
     main()
---
+++ code/subreddit_graph.py
@@ -0,0 +1,18 @@
+"""
+Generates subreddit graph
+"""
+
+def topic_graph(): 
+    """
+    Subreddits by topic similarity 
+    Maybe post similarity, 
+    post title similarity, 
+    or description similarity
+    """
+
+def main():
+    subscriber_graph()
+    topic_graph()
+
+if __name__ == "__main__":
+    main()
